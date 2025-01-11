8281da59dc96f3dba53352fadd422027568ef2ab
LanceRuzel <lanceruzel2@gmail.com>
2025-01-11 08:54:56 +0800

    added auth store and modified sign in view logic

--- resources/js/views/SignInView.vue
+++ resources/js/views/SignInView.vue
@@ -4,6 +4,22 @@ import InputText from 'primevue/inputtext';
 import Password from 'primevue/password';
 import Button from 'primevue/button';
 import Message from 'primevue/message';
+import { onMounted, reactive } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from './../stores/auth';
+
+const authStore = useAuthStore();
+const { authenticate } = authStore;
+const { errors } = storeToRefs(authStore);
+
+const formData = reactive({
+    email: '',
+    password: ''
+});
+
+onMounted(() => {
+    errors.value = {};
+});
 </script>
 
 <template>
@@ -14,26 +30,26 @@ import Message from 'primevue/message';
             </template>
 
             <template #content>
-                <Message class="mt-2" severity="success">fdsf</Message>
+                <Message class="mt-2" :severity="authStore.message.status" v-if="authStore.message">{{ authStore.message.detail }}</Message>
                 
-                <form>
+                <form @submit.prevent="authenticate('login', formData)">
                     <div class="flex flex-col gap-1 mt-3">
                         <label for="email">Email</label>
-                        <InputText type="email"/>
-                        <small class="form-error-message"></small>
+                        <InputText type="email" v-model="formData.email" :invalid="errors.email"/>
+                        <small class="form-error-message" v-if="errors.email">{{ errors.email[0] }}</small>
                     </div>
 
                     <div class="flex flex-col gap-1 mt-3">
                         <label for="password">Password</label>
-                        <Password toggleMask fluid />
-                        <small class="form-error-message"></small>
+                        <Password :feedback="false" v-model="formData.password" toggleMask fluid :invalid="errors.password" />
+                        <small class="form-error-message" v-if="errors.password">{{ errors.password[0] }}</small>
                     </div>
 
                     <!-- <div class="flex justify-end">
                         <Button label="Forgot Password?" link />
                     </div> -->
 
-                    <Button type="submit" label="Signin" class="mt-5" fluid />
+                    <Button type="submit" label="Signin" class="mt-5" fluid :loading="authStore.isLoading" />
                 </form>
 
                 <Button label="Don't have an account? Sign Up" link fluid as="router-link" :to="{ name: 'signup' }" />
